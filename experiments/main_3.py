ad1006d566f2f62767e4204ad643b33a4b61eebd
manzar96 <manoszar96@gmail.com>
2022-02-14 15:45:02 +0200

    updated exps

--- experiments/main_3.py
+++ experiments/main_3.py
@@ -41,7 +41,7 @@ metrics = MetricsWraper(['accuracy'])
 trainer = ClassificationTrainer(model=model, optimizer=optimizer,
                                 config=config, metrics=metrics)
 
-trainer.kfoldvalidation(k_folds=2, epochs=2, dataset=dataset_train,
+trainer.kfoldvalidation(k_folds=5, epochs=config.epochs, dataset=dataset_train,
                         collator_fn=collator_fn,
                         batch_size=config.batch_size)
 
